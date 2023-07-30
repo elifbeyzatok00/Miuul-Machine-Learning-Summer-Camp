d0b7d0dc9fc68566d089dce323cd27e99c80bf48
Elif Beyza Tok <102792446+elifbeyzatok00@users.noreply.github.com>
2023-07-30 10:21:31 +0300

    Add files via upload

---
+++ Modul 1 (week 1 & week 2)/1-Subjects/1-working_environmet_settings/setting_up_working_environment.py
@@ -0,0 +1,71 @@
+###############################################
+# Sayılar (Numbers) ve Karakter Dizileri (Strings)
+###############################################
+
+print("Hello world")
+print("Hello AI Era")
+
+print(9)
+9.2
+type(9.2)
+type("Mrb")
+
+###############################################
+# Atamalar ve Değişkenler (Assignments & Variables)
+###############################################
+a = 9
+a
+
+b = "hello ai era"
+b
+
+c = 10
+a * c
+a * 10
+d = a - c
+
+###############################################
+# Virtual Environment (Sanal Ortam)  ve (Package Managment) Paket Yönetimi
+###############################################
+
+# Sanal ortamların listelenmesi:
+# conda env list
+
+# Sanal ortam oluşturma:
+# conda create –n myenv
+
+# Sanal ortamı aktif etme:
+# conda activate myenv
+
+# Yüklü paketlerin listelenmesi:
+# conda list
+
+# Paket yükleme:
+# conda install numpy
+
+# Aynı anda birden fazla paket yükleme:
+# conda install numpy scipy pandas
+
+# Paket silme:
+# conda remove package_name
+
+# Belirli bir versiyona göre paket yükleme:
+# conda install numpy=1.20.1
+
+# Paket yükseltme:
+# conda upgrade numpy
+
+# Tüm paketlerin yükseltilmesi:
+# conda upgrade –all
+
+# pip: pypi (python package index) paket yönetim aracı
+
+# Paket yükleme:
+# pip install pandas
+
+# Paket yükleme versiyona göre:
+# pip install pandas==1.2.1
+
+
+
+
